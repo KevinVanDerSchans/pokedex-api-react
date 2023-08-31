14d02751c38312695e8a95b6dd3f0cdbfaca8668
Kevin Schans <kevinvdsd@hotmail.com>
2023-08-31 04:36:45 +0200

    Start with pokemons cards

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import Link from "react-router-dom"
+// import Link from "react-router-dom"
 import { HeaderStyled } from "./HeaderStyled"
 
 import './Header.css'
@@ -6,9 +6,9 @@ import './Header.css'
 const Header = () => {
   return (
     <HeaderStyled>
-      <Link to={'/'}>
+
         <img src="/assets/pokemon-logo.svg" alt="Pokemon Logo" />
-      </Link>
+
 
     </HeaderStyled>
   )
